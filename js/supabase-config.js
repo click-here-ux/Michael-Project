57d4e800744e732a3f1437daf09b3ad1adc20a07
SEVENS22 <arnoazarmando@gmail.com>
2026-05-27 21:35:10 +0200

    Fix Supabase anon key

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -5,7 +5,7 @@
 // Encontre em: Settings > API no seu projeto Supabase
 
 const SUPABASE_URL = 'https://ygdhuhntpqqcgzldwfif.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_sUybwt2rbLY0P6snoT7fIA_vU9ymq35';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlnZGh1aG50cHFxY2d6bGR3ZmlmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk4OTg4MzUsImV4cCI6MjA5NTQ3NDgzNX0.yhB388Y5toAnQa7gsJ3izOqlmerbltZ377xP2xVX5xY';
 
 // Inicializar o cliente Supabase
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
